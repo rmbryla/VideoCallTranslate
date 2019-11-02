d948e5a83de9dc758b68b6375fb5f43d288387c9
Ryan Bryla <rmbryla@gmail.com>
2019-11-02 14:44:44 -0400

    adds translate function, xml activity for translate test screen and translate activity class

---
+++ app/src/main/java/com/example/videocalltranslate/TranslateActivity.kt
@@ -0,0 +1,12 @@
+package com.example.videocalltranslate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TranslateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
