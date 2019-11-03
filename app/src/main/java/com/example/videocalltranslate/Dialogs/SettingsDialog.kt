4daac6b94d6f96ca607c75d8801d1880cda0f71f
Ryan Bryla <rmbryla@gmail.com>
2019-11-02 22:25:37 -0400

    changed spinner in settings to button

--- app/src/main/java/com/example/videocalltranslate/Dialogs/SettingsDialog.kt
+++ app/src/main/java/com/example/videocalltranslate/Dialogs/SettingsDialog.kt
@@ -8,13 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import androidx.fragment.app.DialogFragment
 import com.example.videocalltranslate.R
 
 class SettingsDialog : DialogFragment() {
 
-    lateinit var languageSpinner: Spinner
+    lateinit var startOverlayButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,23 +30,8 @@ class SettingsDialog : DialogFragment() {
         val inflater : LayoutInflater = activity!!.layoutInflater
         val view : View = inflater.inflate(R.layout.dialog_settings,null)
 
-        languageSpinner = view.findViewById(R.id.settings_language_spinner)
-        ArrayAdapter.createFromResource(context, R.array.languages, android.R.layout.simple_spinner_item).also{ adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            languageSpinner.adapter=adapter
-        }
+        startOverlayButton = view.findViewById(R.id.start_overlay_button)
 
-        languageSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                // TODO("Change language") //To change body of created functions use File | Settings | File Templates.
-            }
-        }
 
         builder.setView(view).setTitle("Settings").setNegativeButton("Cancel", object : DialogInterface.OnClickListener {
             override fun onClick(dialog: DialogInterface?, which: Int) {
